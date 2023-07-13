24be9e7b96ded60667941ec67460d3b7fc927f55
Wilmer-Buten <wilmer.buten@gmail.com>
2023-07-13 11:39:21 -0400

    Backend para producciom

--- src/config.js
+++
@@ -1,9 +0,0 @@
-
-export default {
-
-    MONGO_DATABASE: process.env.MONGO_DATABASE || 'books-gestion',
-    MONGO_USER: 'admin', 
-    MONGO_PASSWORD: 'password',
-    MONGO_HOST: '127.0.0.1',
-    SECRET: 'JWT_SECRET_PASSWORD'
-}
